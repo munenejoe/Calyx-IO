bcfb34f8d594f9bb02391e5011b018f0100a7318
munenejoe <joemetha97@gmail.com>
2026-06-30 03:25:49 +0300

    chore: clean up project configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 import glsl from "vite-plugin-glsl";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -16,11 +17,18 @@ export default defineConfig(({ mode }) => ({
     },
   },
 
-  // plugins: [
-  //   react(),
-  //   glsl(),
-  //   mode === "development" && componentTagger(),
-  // ].filter(Boolean),
+  plugins: [
+    react(),
+    glsl(),
+    (mode === "development" ? [componentTagger()] : []),
+    (mode === "analyze" ?
+      [visualizer({
+        filename: "dist/stats.html",
+        open: true,
+        gzipSize: true,
+        brotliSize: true,
+      })] : []),
+  ].filter(Boolean),
 
   resolve: {
     alias: {
