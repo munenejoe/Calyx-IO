931c595508636e978856ba48fc48a4bbbf9e3de3
munenejoe <joemetha97@gmail.com>
2026-06-30 03:47:02 +0300

    remove visualizer debug from vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 import glsl from "vite-plugin-glsl";
-import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -17,18 +16,11 @@ export default defineConfig(({ mode }) => ({
     },
   },
 
-  plugins: [
-    react(),
-    glsl(),
-    (mode === "development" ? [componentTagger()] : []),
-    (mode === "analyze" ?
-      [visualizer({
-        filename: "dist/stats.html",
-        open: true,
-        gzipSize: true,
-        brotliSize: true,
-      })] : []),
-  ].filter(Boolean),
+plugins: [
+  react(),
+  glsl(),
+  ...(mode === "development" ? [componentTagger()] : []),
+].filter(Boolean),
 
   resolve: {
     alias: {
